2225cc9f853e8f6a68487a0b9bd154f81c8c8783
Chin-Yuan Hsu <chinyuan@usc.edu>
2018-07-27 11:29:49 -0400

    create user model with all necessary variables and functions

---
+++ MacroFit/Model/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  MacroFit
+//
+//  Created by jeff hsu on 7/20/18.
+//  Copyright © 2018 Jeffrey Hsu. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+
+}
